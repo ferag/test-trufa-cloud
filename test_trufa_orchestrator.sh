1654813ae834da65709cffe61a295d2c84c0f0c4
ferag <ferag@live.com>
2017-09-04 17:18:11 +0200

    Update test_trufa_orchestrator.sh

--- test_trufa_orchestrator.sh
+++ test_trufa_orchestrator.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mkdir /data
 cd /data
 wget https://193.146.75.60/web/static/demo_files/reads_left.fq.tar.gz --no-check-certificate
@@ -15,9 +16,9 @@ mkdir ${STAT_FOLDER}fastqc_report
 
 echo `date +%F\ %H:%M:%S` "START FastQC" >> ${OUT_FOLDER}.LOG.txt
 
-reads_in=$READS_FILES
+reads_in=($READS_FILES)
 
-for (i=0; i<${#reads_in[@]}; i++); do
+for ((i=0; i<${#reads_in[@]}; i++)); do
 
     fastqc -t 2  --noextract ${reads_in[ i ]} -o ${STAT_FOLDER}fastqc_report
     echo FASTQC: IN: ${reads_in[ i ]} >> ${OUT_FOLDER}.LOG.txt
