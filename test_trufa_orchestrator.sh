bb794421fd75ad16cd028cdde677855c651c6b46
ferag <ferag@live.com>
2017-09-04 17:07:26 +0200

    Update test_trufa_orchestrator.sh

--- test_trufa_orchestrator.sh
+++ test_trufa_orchestrator.sh
@@ -17,12 +17,12 @@ echo `date +%F\ %H:%M:%S` "START FastQC" >> ${OUT_FOLDER}.LOG.txt
 
 reads_in=$READS_FILES
 
-for ((i=0; i<${#reads_in[@]}; i++)); do
+for (i=0; i<${#reads_in[@]}; i++); do
 
     fastqc -t 2  --noextract ${reads_in[ i ]} -o ${STAT_FOLDER}fastqc_report
     echo FASTQC: IN: ${reads_in[ i ]} >> ${OUT_FOLDER}.LOG.txt
 done
 wait
-
+cat ${OUT_FOLDER}/*.html
 echo `date +%F\ %H:%M:%S` "END FastQC" >> ${OUT_FOLDER}.LOG.txt
 
