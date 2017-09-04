e4ad1570fa249976a78cae14425c0b65eed6af43
Fer Aguilar <ferag@live.com>
2017-09-04 16:03:24 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# test-trufa-cloud
---
+++ test_trufa_orchestrator.sh
@@ -0,0 +1,28 @@
+mkdir /data
+cd /data
+wget https://193.146.75.60/web/static/demo_files/reads_left.fq.tar.gz --no-check-certificate
+wget https://193.146.75.60/web/static/demo_files/reads_right.fq.tar.gz --no-check-certificate
+
+tar -zxvf ./reads_left.fq.tar.gz
+tar -zxvf ./reads_right.fq.tar.gz
+
+export READS_FILES='/data/reads_left.fq /data/reads_right.fq'
+export OUT_FOLDER='/data/'
+export STAT_FOLDER='/data/'
+
+
+mkdir ${STAT_FOLDER}fastqc_report
+
+echo `date +%F\ %H:%M:%S` "START FastQC" >> ${OUT_FOLDER}.LOG.txt
+
+reads_in=($READS_FILES)
+
+for ((i=0; i<${#reads_in[@]}; i++)); do
+
+    fastqc -t 2  --noextract ${reads_in[ i ]} -o ${STAT_FOLDER}fastqc_report
+    echo FASTQC: IN: ${reads_in[ i ]} >> ${OUT_FOLDER}.LOG.txt
+done
+wait
+
+echo `date +%F\ %H:%M:%S` "END FastQC" >> ${OUT_FOLDER}.LOG.txt
+
